5c91d874cd19418abd26b1e7293c3576c95cdf4a
rajmalpure <malpure.raj@kalvium.community>
2024-01-20 15:56:06 +0530

    pushed

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
   function backgroundChange(){
     setcolor(!color)
     let white='#FFFFFF'
-    let gray='#747474'
+    let violet='#8D79A2'
     let black='#000000'
-    setbgColor(color?gray:white)
+    setbgColor(color?violet:white)
     settextColor(color?white:black)
   }
 
@@ -25,7 +25,7 @@ function App() {
       <div className="head">
         <h1 >Kalvium</h1>
         <button onClick={backgroundChange} className="modeBtn">
-           {color ? 'Light' : 'Dark'}
+           {color ? 'Dark' : 'Light'}
         </button>
       </div>
       <div className="questionBox">
@@ -38,54 +38,3 @@ function App() {
 }
 
 export default App;
-
-// return (
-//   <div style={{background: bgColor }} className="body">
-//     <div className="head">
-//       <h1 style={{color: textColor}}>Kalvium</h1>
-//       <button onClick={backgroundchange} className="modeBtn">{color ?'Light':'Dark'}</button>
-//     </div>
-//     <div className="questionBox">
-//       <QuestionBox />
-//     </div>
-    
-//   </div>
-
-// );
-
-
-
-// import React, { useState } from "react";
-// import "./App.css";
-// import questions from "./questions";
-// import Result from "./components/Result";
-// import QuestionBox from "./components/QuestionBox";
-
-// const App = () => {
-//   const [color, setColor] = useState(true);
-//   const [bgColor, setBgColor] = useState('');
-//   const [textColor, setTextColor] = useState('');
-
-//   const backgroundChange = () => {
-//     setColor(!color);
-//     const [white, gray, black] = ['#FFFFFF', '#747474', '#000000'];
-//     setBgColor(color ? gray : white);
-//     setTextColor(color ? white : black);
-//   };
-
-//   return (
-//     <div style={{ background: bgColor }} className="body">
-//       <div className="head">
-//         <h1 style={{ color: textColor }}>Kalvium</h1>
-//         <button onClick={backgroundChange} className="modeBtn">
-//           {color ? 'Light' : 'Dark'}
-//         </button>
-//       </div>
-//       <div className="questionBox">
-//         <QuestionBox />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,18 +1,3 @@
-// import React, { useContext } from 'react'
-// import {marks} from './QuestionBox.js'
-
-// export default function Result() {
-
-//   return (
-//     <div>
-//       <h1>Final Score</h1>
-//       <h2> out of 5</h2>
-//       <h2></h2>
-//       <button className='restart'>Restart</button>
-//     </div>
-//   )
-// }
-
 import React , {useContext} from 'react'
 import { marks } from './QuestionBox.js'
 
@@ -35,26 +20,3 @@ export default function Result() {
     </div>
   )
 }
-
-// import React , {useContext} from 'react'
-// import { marks } from './QuestionBox.js'
-
-// export default function Result() {
-
-//   const score=useContext(marks)
-
-//   function restart(){
-//     <>
-//       {window.location.reload(false)}
-//     </>
-//   }
-
-//   return (
-//     <div>
-//       <h1>Final Score</h1>
-//       <h2>{score} out of 5</h2>
-//       <h2>({(score/5)*100}%)</h2>
-//       <button onClick={restart} className='restart'>Restart</button>
-//     </div>
-//   )
-// }
